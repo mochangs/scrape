84dca24fccc5e55b4b0823f025a7672821036fd7
lcxiao <112179392+mochangs@users.noreply.github.com>
2025-10-06 09:26:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import aiohttp
 import asyncio
-from telegram import Bot
-token='7168200642:AAGB0oVdKkAZ-Qztqech5PxEEdojstw0P30'
-channel_id=-1002989732424
-bot=Bot(token=token)
-async def send():
-    await bot.send_message(chat_id=channel_id,text='你好',read_timeout=30,write_timeout=30,pool_timeout=30)
-asyncio.run(send())
+PAGE='https://nhentai.net/'
+async def fetch():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(PAGE) as resp:
+            print(resp.status)
+            print(await resp.text())
+asyncio.run(fetch())
